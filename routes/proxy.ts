f7643afbebd278b311b4a4664fcdf636586e1407
이태용 <asst2312010@tosspartners.com>
2024-02-25 22:35:36 +0900

    ky 라이브러리를 사용한 proxy 요청을 한다

--- routes/proxy.ts
+++ routes/proxy.ts
@@ -1,16 +1,26 @@
-import {FastifyInstance, FastifyReply, FastifyRequest} from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 type ProxyRequest = FastifyRequest<{
-    Querystring: { payload: any }
+    Querystring: {
+        target: string;
+        path?: string;
+    }
 }>;
 
 export async function Init(fastify: FastifyInstance) {
-    fastify.get('/redirect', async (request: ProxyRequest, reply: FastifyReply) => {
-        console.log('request.query.payload:"', request.query.payload)
-        const { payload } = request.query
-        // const url = new URL()
+    fastify.get('/api/proxy', async (request: ProxyRequest, reply: FastifyReply) => {
+        const { target, path } = request.query;
+        const url = new URL(`${target}${path ?? ''}`);
 
-        return reply.redirect(302, 'https://fastify.dev');
+        try {
+            const ky = await import('ky');
+
+            const response = await ky.default.get(url.toString()).text();
+            reply.status(200).send(response);
+        } catch (error) {
+            console.error(error);
+            reply.status(500).send({ message: 'error' });
+        }
     });
 
     fastify.get('/a', async (request, reply) => {
