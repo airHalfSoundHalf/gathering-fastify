b987701fbe64e60d06a31a186646de8bea084e74
이태용 <asst2312010@tosspartners.com>
2024-02-27 03:18:27 +0900

    API 프록시 서버 요청

--- routes/proxy.ts
+++ routes/proxy.ts
@@ -1,29 +1,66 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+// @ts-ignore
+import got from 'got'
+import { IncomingHttpHeaders } from "http2";
 
-type ProxyRequest = FastifyRequest<{
-    Querystring: {
-        target: string;
-        path?: string;
-    }
-}>;
+type ProxyRequest = {
+    target: string;
+} & FastifyRequest
+
+const ACCESS_TOKEN = 'test'
 
 export async function Init(fastify: FastifyInstance) {
-    fastify.get('/api/proxy', async (request: ProxyRequest, reply: FastifyReply) => {
-        const { target, path } = request.query;
-        const url = new URL(`${target}${path ?? ''}`);
+    fastify.get('/api/proxy', async (request, reply: FastifyReply) => {
+        const { target } = request.query as ProxyRequest;
+        const url: URL = new URL(target);
 
         try {
-            const ky = await import('ky');
-
-            const response = await ky.default.get(url.toString()).text();
+            const response = await got(url).text();
+            console.log('response:', response)
             reply.status(200).send(response);
         } catch (error) {
-            console.error(error);
-            reply.status(500).send({ message: 'error' });
+            console.error('error:', error);
+            reply.status(500).send('error');
         }
     });
 
-    fastify.get('/a', async (request, reply) => {
-        return 'this is a';
+    fastify.post('/api/proxy', async (request, reply: FastifyReply) => {
+        const { target } = request.query as ProxyRequest;
+
+        const url = new URL(target);
+        console.log('Request URL:', url);
+        const options = {
+            headers: {
+                ...request.headers,
+                'Authorization': `Bearer ${ACCESS_TOKEN}`
+            } as IncomingHttpHeaders
+        }
+
+        const modifyQuery = {
+            searchParams: {
+                ...request.query as ProxyRequest,
+                testParams: '11'
+            }
+        }
+
+        try {
+            const response = await got(url, {
+                ...options,
+                searchParams: modifyQuery.searchParams,
+            }).text()
+            console.log('response:', response)
+            reply.status(200).send(response)
+        } catch (error) {
+            console.error('error:', error)
+            reply.status(500).send('error')
+        }
+    });
+
+    // 리다이렉트
+    fastify.get('/api/test*', async (request, reply) => {
+        if(request.url.startsWith('/api/test')) {
+            reply.redirect('/api')
+            return
+        }
     });
 }
