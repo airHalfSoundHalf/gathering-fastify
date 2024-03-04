7a553754d3551cf61181767cccaa73893b6adcc0
tylee <bpthess@naver.com>
2024-03-05 02:50:37 +0900

    sqlite를 설치해 DB를 연결하는 작업을 한다

---
+++ db/index.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+import fastifySqlite from "fastify-sqlite";
+
+async function main () {
+    const app = fastify()
+    app.register(fastifySqlite, {
+        dbFile: 'foo.db'
+    })
+    await app.ready()
+
+    app.all('SELECT * FROM myTable', (err, rows) => {
+    })
+}
--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { ProxyInit } from './proxy';
+import { DBConnect } from './connect';
 
 export async function Routes(fastify: FastifyInstance) {
     fastify.register(ProxyInit)
+    fastify.register(DBConnect)
 }
